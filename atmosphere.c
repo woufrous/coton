55042cd9375173c150da940ded7cffcefd30f328
Hans Grob <H.Grob@physik.uni-muenchen.de>
2015-03-26 13:44:26 +0100

    atmos: new Scatterer implementation

--- atmosphere.c
+++ atmosphere.c
@@ -15,13 +15,10 @@
 double rayleighPDF(double r)
 {
 	double u=0, d=0, q=0;
-
 	NORM_R(r);
-
 	q = -8*r + 4;
 	d = 1 + (pow(q, 2) / 4.);
 	u = pow(-(q/2.) + sqrt(d), 1./3.);
-
 	return (u - (1/u));
 }
 
@@ -30,34 +27,27 @@ double henyeyGreensteinPDF(double r)
 	/* hardcode assymetry parameter to 0.85...good enough */
 	double g = 0.85;
 	double u=0, v=0, w=0;
+	NORM_R(r);
 	w = g * (2*r - 1) + 1;
 	v = (1.-pow(g, 2))/w;
 	u = -pow(v, 2) + pow(g, 2) + 1;
-
 	return u/(2*g);
 }
 
 double tauPDF(double r)
 {
+	NORM_R(r);
 	return -log(1.-r);
 }
 
 double azimuthPDF(double r)
 {
+	NORM_R(r);
 	return 2*M_PI*r;
 }
 
 double unityPDF(double r)
 {
 	return r;
-}
-
-Photon* rayleighScatter(Photon* p_in)
-{
-	return p_in;
-}
-
-Photon* henyeyGreensteinScatter(Photon* p_in)
-{
 }
 
--- atmosphere.h
+++ atmosphere.h
@@ -4,21 +4,26 @@
 #include "photon.h"
 #include "linalg.h"
 
-typedef Photon* (*ScatterFunction)(Photon*);
+typedef double (*PropDistFunction)(double);
+
+typedef struct {
+	PropDistFunction pdf;
+	double beta_scat;
+} Scatterer;
 
 typedef struct {
 	Vec3D pos;
 	Vec3D dim;
-	double beta_scat;
 	double beta_abs;
-	ScatterFunction scatter;
+	Scatterer* molecular;
+	Scatterer* cloud;
 } Box;
 
-Photon* rayleighScatter(Photon*);
 double rayleighPDF(double r);
 double henyeyGreensteinPDF(double r);
 double tauPDF(double r);
 double azimuthPDF(double r);
 double unityPDF(double r);
+
 
 #endif // __ATMOSPHERE_H
